d48f7a75382b9686547ba755fc0dc0be39157f9f
Vera Ama Pomaa <verapomaa99@gmail.com>
2026-06-09 08:03:51 +0000

    created and added origins to the cors

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routes.auth import users_router
 from app.routes.admin import admin_router
 from app.routes.rides import ride_router
@@ -8,6 +9,20 @@ from app.routes.passenger import passenger_router
 app = FastAPI(
     title="A CARPOOLING API",
     description="A basic backend for a carpooling project"
+)
+
+origins = [
+    "http://localhost:8081",
+    "http://localhost:8000",
+    "https://urban-lift-api.onrender.com/",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 @app.get("/")
